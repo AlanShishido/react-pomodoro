1d1c5863404ec95646d6d7c8a59f4f6cb04c606e
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2024-01-25 09:01:01 -0400

    Timer component

---
+++ src/components/Timer.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+interface TimerContainerProps {}
+
+export const TimerContainer = styled.time<TimerContainerProps>`
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+`
---
+++ src/components/Timer.tsx
@@ -0,0 +1,3 @@
+export function Timer() {
+  return <h1>Testando</h1>
+}
